5716d0cedb1ea0101f254bab82edfdbb08284031
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-07 05:34:38 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as Font from 'expo-font';
+import { useFonts } from '@use-expo/font';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import { Article, BlogPosts } from './src/screens';
+import Header from './src/components/blogposts/Header';
+import ArticleHeader from './src/components/article/ArticleHeader'
+import blogs from './src/blogs';
+
+
+const theme = {
+	...DefaultTheme,
+	colors: {
+		...DefaultTheme.colors,
+		background: '#fff'
+	}
+}
+const Stack = createStackNavigator()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+	const [loaded] = useFonts({
+		'Crimson': require('./assets/fonts/CrimsonText-Regular.ttf'),
+		'CrimsonBold': require('./assets/fonts/CrimsonText-Bold.ttf'),
+		'CrimsonSemi': require('./assets/fonts/CrimsonText-SemiBold.ttf'),
+	})
+
+  return !loaded ? null :  (
+  	<NavigationContainer theme={theme}>
+  		<Stack.Navigator>
+  			<Stack.Screen
+  				name='blogposts'
+  				component={BlogPosts}
+  				options={{
+  					header: props => <Header/>
+  				}}
+  			/>
+  			<Stack.Screen
+  				name='article'
+  				component={Article}
+  				options={{
+  					header: ({navigation, scene: {route}}) => (
+  						<ArticleHeader navigation={navigation} route={route}/>
+  					)
+  				}}
+  			/>
+  		</Stack.Navigator>
+  	</NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
