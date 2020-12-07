ef7c820f0dc647697a7e9132b7b0a9a3e8d2f4ab
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-07 05:29:55 +0000

    Update utils.js

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+const colors = {
+	primary: '#324558',
+	primaryLight: '#99a3ab',
+	secondary: '#f0f4f4',
+	white: '#fff',
+};
+
+const vars = {
+	gap: {
+		xs: 5,
+		s: 15,
+		m: 40,
+		l: 75,
+		xl: 120,
+	}
+};
+
+export {
+	colors,
+	vars
+};
