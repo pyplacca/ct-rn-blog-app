fdd110668be92b8440d45280c97d7f05fd7dd363
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-08 19:23:54 +0000

    add "pull to refresh" for no particular reason

--- src/screens/BlogPosts.jsx
+++ src/screens/BlogPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, Text, StyleSheet, StatusBar } from 'react-native';
 import PostCard from '../components/blogposts/PostCard';
 import { BottomTabs } from '../components'
@@ -7,9 +7,23 @@ import blogs from '../blogs';
 
 
 function BlogPosts (props) {
+	const [refresh, setRefresh] = useState(false)
+
 	return (
 		<View style={styles.container}>
 			<FlatList
+				/*
+					This refresh stuff does nothing really. I was only trying it out.
+					Swipe down in the first page of the app, and you'll see
+						a refresh icon pop out from the top
+				*/
+				onRefresh={arg => {
+					console.log(arg)
+					setRefresh(true)
+					setTimeout(() => setRefresh(false), 2000)
+				}}
+				refreshing={refresh}
+				// and it ends right above this line
 				data={blogs}
 				renderItem={
 					({item}) => <PostCard {...item} navigation={props.navigation} />
